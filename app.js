c6c7135ffb6cb4543dbacce0603b3ae61cdfa37a
Anand Alex <anandalex1993@gmail.com>
2019-08-20 16:18:59 +0530

    employee insertion and search

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const request = require('request');
+
+
+const app = express();
+app.set('view engine','ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/empDB");
+
+const employeeform = mongoose.model("employeedetails",{
+    name:String,
+    desi:String,
+    salary:String
+
+});
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.post("/",(req,res)=>{
+    var employee = employeeform(req.body);
+    var result = employee.save((error)=>{
+        if(error){
+            throw error;
+        }else{
+            console.log("user added");
+        }
+    });
+});
+
+app.get("/getdata",(req,res)=>{
+    employeeform.find().exec((error,data)=>{
+        console.log(data);
+        res.send(data);
+    });
+});
+
+app.get('/searchEmployee',(req,res)=>{
+    res.render("findemp");
+});
+
+app.get("/searchEmp/:name",(req,res)=>{
+    var x= req.params.name;
+    employeeform.find({name:x},(error,data)=>{
+        if(error){
+            throw error
+        }else{
+            res.send(data)
+        }
+    });
+});
+
+
+
+const api="http://localhost:3000/getdata";
+
+app.get("/view",(req,res)=>{
+    request(api,(error,response,body)=>{
+        var data = JSON.parse(body);
+        console.log(data);
+        res.render("viewemployee",{data:data});
+    });
+    
+});
+
+const searchapi = "http://localhost:3000/searchEmp/";
+
+app.post("/find",(req,res)=>{
+    var x=req.body.name;
+    console.log(x);
+   
+    request(searchapi+x,(error,response,body)=>{
+        var mem = JSON.parse(body);
+        res.render("viewemployee",{data:mem});
+    })
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("server is up and listening!");
+});
